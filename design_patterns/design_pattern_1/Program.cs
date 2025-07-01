8fc6808f42f23948a7345a82483ed332901ed99d
gokcedeniz <152417153+gokcedeniz@users.noreply.github.com>
2025-07-01 14:11:23 +0300

    Add files via upload

---
+++ design_patterns/design_pattern_1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//creational patterns: singleton
+//default constructor is private
+using System;
+
+public class Counter
+{
+  
+    private static Counter _instance;
+
+    //Sayaç değişkeni
+    private static int _count = 0;
+
+    // constructor private olmalı
+    private Counter() { }
+
+    //Singleton'a dışarıdan erişim için static property
+    public static Counter Instance
+    {
+        get
+        {
+            // Eğer nesne daha önce oluşturulmadıysa oluştur
+            if (_instance == null)
+                _instance = new Counter();
+
+            // Aynı nesneyi döndür
+            return _instance;
+        }
+    }
+
+    //Sayacı artıran metot
+    public void Increment()
+    {
+        _count++;
+    }
+
+    //Sayacın değerini döndürür
+    public int GetCount()
+    {
+        return _count;
+    }
+}
+
+namespace design_pattern_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            Console.WriteLine("Başlangıç sayısı: " + Counter.Instance.GetCount());
+
+            //Sayacı artır
+            Counter.Instance.Increment();
+            Counter.Instance.Increment();
+
+            Console.WriteLine("Artış sonrası: " + Counter.Instance.GetCount());
+
+            // Tekrar başka yerden erişim
+            var c = Counter.Instance;
+            c.Increment();
+
+            Console.WriteLine("Son değer: " + c.GetCount()); 
+        }
+    }
+}
+
---
+++ design_patterns/design_pattern_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//creational patterns: Factory method
+public abstract class Animal
+{
+    public abstract void Move();
+}
+
+public class Dog : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Walking!");
+    }
+}
+
+public class Bird : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Flying!");
+    }
+}
+
+public class AnimalFactory
+{
+    public static Animal GetAnimal(string type)
+    {
+        if (type == "dog") return new Dog();
+        if (type == "bird") return new Bird();
+        return null;
+    }
+}
+namespace design_pattern_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = AnimalFactory.GetAnimal("dog");
+            animal1.Move(); 
+
+            Animal animal2 = AnimalFactory.GetAnimal("bird");
+            animal2.Move();
+
+            Console.ReadLine();
+        }
+    }
+}
+
